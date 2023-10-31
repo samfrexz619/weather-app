ae8f3564e47d9f1eeeba12933bcffba34542be63
samfrexz619 <samfrexz69@gmail.com>
2023-10-31 09:01:23 +0100

    feat: update styles on weather component

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -3,14 +3,16 @@ import sunny from '@/assets/images/sunshine.svg'
 
 const WeatherCard = () => {
   return (
-    <div className="h-260 bg-card rounded-30 dark:bg-[#444] shadow-box w-full">
-      <div className="w-95 mx-auto py-4 flex h-full items-center">
+    <div className="min-h-260 py-4 bg-card rounded-30 dark:bg-[#444] shadow-box w-full">
+      <div className="w-95 mx-auto py-4 flex gap-x-5 text-text dark:text-white">
         <div>
-          <h2 className="text-grad">24&#8451;</h2>
+          <h2 className="text-grad bg-txt_lit font-bold text-[72px] dark:bg-txt_dak leading-none">24&#8451;
+          </h2>
+          <small>Feels like: <span>22&#8451;</span></small>
         </div>
-        <div className='text-center'>
-          <img src={sunny} alt="sun image" className='w-[200px]' />
-          <small>Sunny</small>
+        <div className='text-center h-full'>
+          <img src={sunny} alt="sun image" className='w-[180px]' />
+          <p className='text-3xl font-bold text-center'>Sunny</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ export default {
         poppins: ['Poppins', 'sans-serif']
       },
       boxShadow: {
-        box: '3px 3px 4px 0px rgba(0, 0, 0, 0.50)',
+        box: '2px 2px 2px 0px rgba(0, 0, 0, 0.50)',
       },
       borderRadius: {
         30: '30px'
@@ -35,7 +35,9 @@ export default {
       backgroundImage: {
         orange: 'linear-gradient(171deg, #F88508 -12.41%, rgba(246, 250, 217, 0.00) 163.32%)',
         grey: 'linear-gradient(174deg, #443D64 -15.92%, rgba(101, 130, 198, 0.00) 192.45%);',
-        dak: 'linear-gradient(174deg, #373636, #373636)'
+        dak: 'linear-gradient(174deg, #373636, #373636)',
+        txt_dak: 'linear-gradient(84deg, #FFF -16.56%, rgba(255, 255, 255, 0.00) 118.43%);',
+        txt_lit: '-webkit-linear-gradient(#292929 -2.93%, rgba(255, 255, 255, 0.00))'
       }
     },
   },
