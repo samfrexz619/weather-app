469e6f06b014c962fab011f8387276712c4c68ef
BlessingOse <maryblessing633@gmail.com>
2023-11-09 15:54:35 +0100

    about page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,29 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <main>
+      <p>WeatherApp was inspired by both societal and practical needs.
+        As it is a very important part of our daily lives, WeatherApp is 
+        vital to help plan our daily activities. A lot of people do not use weather apps because
+        they find them either boring or difficult to use. 
+      </p>
+      <p>
+        WeatherApp was inspired to help meet these needs and even provide more.
+      </p>
+      <p>
+        A six-week duration was used to build the WeatherApp. Each week,
+        a part of the app was carefully built and sent to peers for review. 
+      </p>
+
+      <a href="https://intranet.alxswe.com/projects/361">WeatherApp is a Portfolio Project for Holberton School</a>
+      <a href="https://github.com/samfrexz619">Samuel Damilola Oyawale</a>
+      <a href="https://www.linkedin.com/in/samueldamilolaoyawale/">Samuel Damilola Oyawale</a>
+      <a href="https://twitter.com/undefined_619">Samuel Damilola Oyawale</a>
+      <a href="https://github.com/BlessingOse">Abhasihewere Blessing</a>
+      <a href="https://www.linkedin.com/in/blessing-abhasibhewere-036393b0">Abhasihewere Blessing</a>
+      <a href="https://twitter.com/RealJesusB">Abhasihewere Blessing</a>
+      <a href="https://github.com/samfrexz619/weather-app">WeatherApp Github repository</a>
+    </main>
   )
 }
 
